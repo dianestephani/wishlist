10c93c19ff047e71fc3425af7605077ca690ec38
Diane Stephani <diane@onemoregame.com>
2026-03-25 21:26:29 -0700

    new tests pass

--- wishlist_app/wsgi.py
+++ wishlist_app/wsgi.py
@@ -9,10 +9,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wishlist_app.settings")
 
 application = get_wsgi_application()
-
-# Wrap with WhiteNoise to serve media files in production
-from whitenoise import WhiteNoise
-from django.conf import settings
-
-application = WhiteNoise(application, root=settings.STATIC_ROOT)
-application.add_files(settings.MEDIA_ROOT, prefix="media/")

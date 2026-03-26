48364e7e20171ee796c0c633d06e772e82ffe14d
Diane Stephani <diane@onemoregame.com>
2026-03-25 21:37:11 -0700

    new database change

--- wishlist_app/settings.py
+++ wishlist_app/settings.py
@@ -5,6 +5,8 @@ Django settings for wishlist_app project.
 import os
 from pathlib import Path
 
+import dj_database_url
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get(
@@ -61,10 +63,10 @@ TEMPLATES = [
 WSGI_APPLICATION = "wishlist_app.wsgi.application"
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
